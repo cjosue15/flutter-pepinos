23ed4bab305954b86d0d6787a6a56fc2f65c2adf
Carlos Morales <carlosjosuemn98@gmail.com>
2021-04-01 21:57:28 -0500

    refactor(routes): routing app

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:pepinos/src/pages/cliente_detail_page.dart';
-import 'package:pepinos/src/pages/home_page.dart';
+import 'package:pepinos/src/pages/clientes_page.dart';
+import 'package:pepinos/src/pages/ventas_page.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes() {
   return <String, WidgetBuilder>{
-    '/': (BuildContext context) => HomePage(),
-    'cliente': (BuildContext context) => ClienteDetailPage(),
+    '/': (BuildContext context) => VentasPage(),
+    'clientes': (BuildContext context) => ClientesPage(),
+    'clientes/formulario': (BuildContext context) => ClienteDetailPage(),
   };
 }
