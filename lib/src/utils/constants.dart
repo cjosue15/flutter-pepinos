5bf6a45c32c1ffc9b1cdd5a3c76fc098744aec0c
Carlos Morales <carlosjosuemn98@gmail.com>
2021-04-04 14:45:24 -0500

    feat(constants): constants file was created

---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,15 @@
+class Constants {
+  Constants._();
+  // form
+  static const double paddingForm = 15.0;
+  static const double marginTop = 20.0;
+  static const double height = 25.0;
+}
+
+class ConstantsForm {
+  ConstantsForm._();
+  // form
+  static const double padding = 15.0;
+  static const double margin = 20.0;
+  static const double height = 25.0;
+}
