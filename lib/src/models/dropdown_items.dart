014086c5589ad6a80f1333896793f986d16d5821
Carlos Morales <carlosjosuemn98@gmail.com>
2021-04-04 14:40:13 -0500

    refactor(dropdown): new class and funcionallity

---
+++ lib/src/models/dropdown_items.dart
@@ -0,0 +1,5 @@
+class DropdownItem {
+  String id;
+  String text;
+  DropdownItem({this.id, this.text});
+}
--- lib/src/widgets/dropdown.dart
+++ lib/src/widgets/dropdown.dart
@@ -4,14 +4,12 @@ class AppDropdownInput<T> extends StatelessWidget {
   final String hintText;
   final List<T> options;
   final T value;
-  final String Function(T) getLabel;
   final String Function(T) validator;
   final void Function(T) onChanged;
 
   AppDropdownInput({
     this.hintText = 'Please select an Option',
     this.options = const [],
-    this.getLabel,
     this.value,
     this.validator,
     this.onChanged,
@@ -19,29 +17,67 @@ class AppDropdownInput<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FormField<T>(builder: (FormFieldState<T> state) {
-      return InputDecorator(
-        decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
-          labelText: hintText,
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
-        ),
-        isEmpty: value == null || value == '',
-        child: DropdownButtonHideUnderline(
-          child: DropdownButtonFormField<T>(
-            decoration: InputDecoration(border: InputBorder.none),
-            value: value,
-            onChanged: onChanged,
-            validator: validator,
-            items: options.map((T value) {
-              return DropdownMenuItem<T>(
-                value: value,
-                child: Text(getLabel(value)),
-              );
-            }).toList(),
-          ),
-        ),
-      );
-    });
+    return DropdownButtonFormField<T>(
+      decoration: InputDecoration(
+        labelText: hintText,
+        hintText: hintText,
+        border: OutlineInputBorder(),
+      ),
+      value: value,
+      validator: validator,
+      onChanged: onChanged,
+      items: options
+          .map((dynamic item) => DropdownMenuItem<T>(
+                value: item,
+                child: Text(item.text, overflow: TextOverflow.ellipsis),
+              ))
+          .toList(),
+    );
   }
 }
+
+// return FormField<T>(builder: (FormFieldState<T> state) {
+//   return InputDecorator(
+//     decoration: InputDecoration(
+//       // errorText: 'asda',
+//       contentPadding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+//       labelText: hintText,
+//       border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
+//     ),
+//     isEmpty: value == null || value == '',
+//     child: DropdownButtonHideUnderline(
+//       child: DropdownButtonFormField<T>(
+//         decoration: InputDecoration(border: InputBorder.none),
+//         value: value,
+//         onChanged: onChanged,
+//         validator: validator,
+//         items: options.map((T value) {
+//           print(options);
+//           return DropdownMenuItem<T>(
+//             value: value,
+//             child: Text(getLabel(value)),
+//           );
+//         }).toList(),
+//       ),
+//     ),
+//   );
+// }
+
+//  DropdownButtonFormField<String>(
+//               decoration: InputDecoration(
+//                 labelText: _currentItem,
+//                 hintText: 'Option',
+//                 border: OutlineInputBorder(),
+//               ),
+//               value: _currentItem,
+//               validator: (value) => value == null ? 'Ingrese un invernadero' : null,
+//               items: _currencies
+//                   .map((label) => DropdownMenuItem(
+//                         child: Text(label),
+//                         value: label,
+//                       ))
+//                   .toList(),
+//               onChanged: (value) {
+//                 setState(() => _currentItem = value);
+//               },
+//             )
