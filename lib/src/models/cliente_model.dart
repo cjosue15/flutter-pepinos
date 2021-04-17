27ea689a280418584342d717389895ad01c5a857
Carlos Morales <carlosjosuemn98@gmail.com>
2021-04-17 17:07:34 -0500

    update client model

--- lib/src/models/cliente_model.dart
+++ lib/src/models/cliente_model.dart
@@ -18,41 +18,34 @@ Cliente clienteFromJson(String str) => Cliente.fromJson(json.decode(str));
 String clienteToJson(Cliente data) => json.encode(data.toJson());
 
 class Cliente {
-  Cliente({
-    this.idCliente,
-    this.nombres,
-    this.apellidos,
-    this.lugar,
-    this.puesto,
-    // this.createdAt,
-    // this.updatedAt,
-  });
+  Cliente(
+      {this.idCliente,
+      this.nombres,
+      this.apellidos,
+      this.lugar,
+      this.puesto,
+      this.idEstado = 1});
 
   int idCliente;
   String nombres;
   String apellidos;
   String lugar;
   String puesto;
-  // String createdAt;
-  // String updatedAt;
+  int idEstado;
 
   factory Cliente.fromJson(Map<String, dynamic> json) => Cliente(
-        idCliente: json["id_cliente"],
-        nombres: json["nombres"],
-        apellidos: json["apellidos"],
-        lugar: json["lugar"],
-        puesto: json["puesto"],
-        // createdAt: json["createdAt"],
-        // updatedAt: json["updatedAt"],
-      );
+      idCliente: json["id_cliente"],
+      nombres: json["nombres"],
+      apellidos: json["apellidos"],
+      lugar: json["lugar"],
+      puesto: json["puesto"],
+      idEstado: json.containsKey("id_estado") ? json["id_estado"] : null);
 
   Map<String, dynamic> toJson() => {
-        // "id_cliente": idCliente,
         "nombres": nombres,
         "apellidos": apellidos,
         "lugar": lugar,
         "puesto": puesto,
-        // "createdAt": createdAt,
-        // "updatedAt": updatedAt,
+        "id_estado": idEstado
       };
 }
