6d1350c20dbee20734352928ef15a852ca509db5
Carlos Morales <carlosjosuemn98@gmail.com>
2021-04-25 21:46:09 -0500

    refactor(routes): ventas routes of files was updated

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,13 +5,15 @@ import 'package:pepinos/src/pages/invernaderos/invernadero_form_page.dart';
 import 'package:pepinos/src/pages/invernaderos/invernadero_list_page.dart';
 import 'package:pepinos/src/pages/productos/productos_form_page.dart';
 import 'package:pepinos/src/pages/productos/productos_list_page.dart';
-import 'package:pepinos/src/pages/ventas_form.dart';
-import 'package:pepinos/src/pages/ventas_page.dart';
+import 'package:pepinos/src/pages/ventas/ventas_detail_page.dart';
+import 'package:pepinos/src/pages/ventas/ventas_form_page.dart';
+import 'package:pepinos/src/pages/ventas/ventas_list_page.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => VentasPage(),
     'ventas/form': (BuildContext context) => VentasForm(),
+    'ventas/detail': (BuildContext context) => VentasDetailPage(),
     'clientes': (BuildContext context) => ClientesPage(),
     'clientes/form': (BuildContext context) => ClienteDetailPage(),
     'productos': (BuildContext context) => ProductosListPage(),
