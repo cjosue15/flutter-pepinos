05be37a41d37f00fdb4dd4ad225a372f86792f15
Carlos Morales <carlosjosuemn98@gmail.com>
2021-04-25 21:42:04 -0500

    feat(validator): number validator non zero

--- lib/src/utils/utils_validatos.dart
+++ lib/src/utils/utils_validatos.dart
@@ -7,3 +7,9 @@ bool isNumeric(String s) {
 String isTextEmpty({String value, int length, String message}) {
   return value.isEmpty || value.length < length ? message : null;
 }
+
+String isNumberEmpty({String value, String message}) {
+  final numberToConvert = value.replaceAll(RegExp('S/'), '');
+  final number = double.parse(numberToConvert);
+  return number == 0 ? message : null;
+}
