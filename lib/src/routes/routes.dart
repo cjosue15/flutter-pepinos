b2480f60090621d7ff23490bd51c4e3d583c9e8f
Carlos Morales <carlosjosuemn98@gmail.com>
2021-04-26 18:29:04 -0500

    refactor(route): change name of file cliente

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pepinos/src/pages/cliente_detail_page.dart';
-import 'package:pepinos/src/pages/clientes_page.dart';
+import 'package:pepinos/src/pages/clientes/cliente_form_page.dart';
+import 'package:pepinos/src/pages/clientes/clientes_list_page.dart';
 import 'package:pepinos/src/pages/invernaderos/invernadero_form_page.dart';
 import 'package:pepinos/src/pages/invernaderos/invernadero_list_page.dart';
 import 'package:pepinos/src/pages/productos/productos_form_page.dart';
@@ -14,8 +14,8 @@ Map<String, WidgetBuilder> getAplicationRoutes() {
     '/': (BuildContext context) => VentasPage(),
     'ventas/form': (BuildContext context) => VentasForm(),
     'ventas/detail': (BuildContext context) => VentasDetailPage(),
-    'clientes': (BuildContext context) => ClientesPage(),
-    'clientes/form': (BuildContext context) => ClienteDetailPage(),
+    'clientes': (BuildContext context) => ClientesListPage(),
+    'clientes/form': (BuildContext context) => ClienteFormPage(),
     'productos': (BuildContext context) => ProductosListPage(),
     'productos/form': (BuildContext context) => ProductsFormPage(),
     'invernaderos': (BuildContext context) => InvernaderoListPage(),
