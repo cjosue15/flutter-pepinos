1e51a2cb5e6066c752abe79583249a209eef7d5b
Carlos Morales <carlosjosuemn98@gmail.com>
2021-05-02 15:47:26 -0500

    refactor isPriceGreaterThanZero

--- lib/src/utils/utils_validatos.dart
+++ lib/src/utils/utils_validatos.dart
@@ -8,8 +8,5 @@ String isTextEmpty({String value, int length, String message}) {
   return value.isEmpty || value.length < length ? message : null;
 }
 
-String isNumberEmpty({String value, String message}) {
-  final numberToConvert = value.replaceAll(RegExp('S/'), '');
-  final number = double.parse(numberToConvert);
-  return number == 0 ? message : null;
-}
+String isPriceGreaterThanZero({String value, String message}) =>
+    double.parse(value) == 0 ? message : null;
