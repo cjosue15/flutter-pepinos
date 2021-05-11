c626952ad5106a3f7385d568f3da9aa868e341fe
Carlos Morales <carlosjosuemn98@gmail.com>
2021-05-11 18:10:30 -0500

    refactor(date-format): add dateTimeToString

--- lib/src/utils/date_format.dart
+++ lib/src/utils/date_format.dart
@@ -10,8 +10,14 @@ String dateFormatToDatabase(String dateWithOutFormatInString) {
   return _dateFormatToDatabase.format(date);
 }
 
-String dateTimeToString(String date) {
+/// This function recives date yyyy-MM-dd and return dd/MM/yyyy
+String dateFormFromDatabase(String date) {
   DateFormat _dateFormat = DateFormat('dd/MM/yyyy');
   DateFormat _dateFormatFromDatabase = DateFormat('yyyy-MM-dd');
   return _dateFormat.format(_dateFormatFromDatabase.parse(date));
+}
+
+String dateTimeToString({DateTime date, DateFormat dateFormat}) {
+  DateFormat _dateFormat = dateFormat ?? DateFormat('dd/MM/yyyy');
+  return _dateFormat.format(date);
 }
