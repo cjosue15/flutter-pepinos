8272b76195aa6afa7d578756a18ba0d26d390ded
Carlos Morales <carlosjosuemn98@gmail.com>
2021-05-11 18:21:38 -0500

    feat(model): addinvernaderoToJson and invernaderoToJsonRequestPut

--- lib/src/models/invernadero.dart
+++ lib/src/models/invernadero.dart
@@ -9,28 +9,41 @@ Invernadero invernaderoFromJson(String str) =>
 
 String invernaderoToJson(Invernadero data) => json.encode(data.toJson());
 
+String invernaderoToJsonRequestPut(Invernadero data) =>
+    json.encode(data.toJsonPUT());
+
 class Invernadero {
   Invernadero(
       {this.idInvernadero,
       this.nombreInvernadero,
       this.idEstado,
-      this.productosSeleccionados});
+      this.productosSeleccionados,
+      this.productosNoSeleccionados});
 
   int idInvernadero;
   String nombreInvernadero;
   int idEstado;
   List<int> productosSeleccionados;
+  List<int> productosNoSeleccionados;
 
   factory Invernadero.fromJson(Map<String, dynamic> json) => Invernadero(
-      idInvernadero: json["id_invernadero"],
-      nombreInvernadero: json["nombre_invernadero"],
-      idEstado: json["id_estado"],
-      productosSeleccionados: json["productos"].length > 0
-          ? json["productos"].map((item) => item["id_producto"]).toList()
-          : null);
+        idInvernadero: json["id_invernadero"],
+        nombreInvernadero: json["nombre_invernadero"],
+        idEstado: json["id_estado"],
+        productosSeleccionados: json["productos"].length > 0
+            ? List<int>.from(
+                json["productos"].map((item) => item["id_producto"]))
+            : [],
+      );
 
   Map<String, dynamic> toJson() => {
         "nombre_invernadero": nombreInvernadero,
         "productos_seleccionados": productosSeleccionados
+      };
+
+  Map<String, dynamic> toJsonPUT() => {
+        "nombre_invernadero": nombreInvernadero,
+        "productos_seleccionados": productosSeleccionados,
+        "productos_no_seleccionados": productosNoSeleccionados
       };
 }
