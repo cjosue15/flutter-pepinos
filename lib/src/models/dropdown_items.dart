144df71e34be025ecee350d8a23d526e80a37a1d
Carlos Morales <carlosjosuemn98@gmail.com>
2021-05-11 18:03:34 -0500

    refactor(dropdown): Widget modified, added hashCode and operator==

--- lib/src/models/dropdown_items.dart
+++ lib/src/models/dropdown_items.dart
@@ -1,24 +1,3 @@
-// class DropdownItem {
-//   int id;
-//   String text;
-//   List<DropdownItem> items = [];
-
-//   DropdownItem({this.id, this.text});
-
-//   factory DropdownItem.fromJsonMap(
-//           {Map<String, dynamic> json, String idValue, String textValue}) =>
-//       DropdownItem(id: json[idValue], text: json[textValue]);
-
-//   DropdownItem.fromJsonList(
-//       {List<dynamic> jsonList, String idValue, String textValue}) {
-//     if (jsonList == null) return;
-
-//     for (final item in jsonList) {
-//       this.items.add(new DropdownItem.fromJsonMap(
-//           json: item, idValue: idValue, textValue: textValue));
-//     }
-//   }
-//
 class DropdownItem {
   int id;
   String text;
@@ -26,27 +5,10 @@ class DropdownItem {
 
   DropdownItem({this.id, this.text});
 
+  int get hashCode => id.hashCode;
+  bool operator ==(Object other) => other is DropdownItem && other.id == id;
+
   factory DropdownItem.fromJsonMap(
           {Map<String, dynamic> json, int idValue, String textValue}) =>
       DropdownItem(id: idValue, text: textValue);
-
-  // DropdownItem.fromJsonList(
-  //     {List<dynamic> jsonList, String idValue, String textValue}) {
-  //   if (jsonList == null) return;
-
-  //   for (final item in jsonList) {
-  //     this.items.add(new DropdownItem.fromJsonMap(
-  //         json: item, idValue: idValue, textValue: textValue));
-  //   }
-  // }
-
-  // Map<String, dynamic> toJson() => {
-  //       // "id_cliente": idCliente,
-  //       "nombres": nombres,
-  //       "apellidos": apellidos,
-  //       "lugar": lugar,
-  //       "puesto": puesto,
-  //       // "createdAt": createdAt,
-  //       // "updatedAt": updatedAt,
-  //     };
 }
