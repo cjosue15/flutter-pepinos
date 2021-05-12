fbc3e5ebb88d5b4e9362ff526c1e4c255c4249bc
Carlos Morales <carlosjosuemn98@gmail.com>
2021-05-12 18:13:59 -0500

    add InvernaderoFilter

--- lib/src/models/invernadero.dart
+++ lib/src/models/invernadero.dart
@@ -25,15 +25,25 @@ class Invernadero {
   int idEstado;
   List<int> productosSeleccionados;
   List<int> productosNoSeleccionados;
+  List<Invernadero> items = [];
+
+  Invernadero.fromJsonList({List<dynamic> jsonList}) {
+    if (jsonList == null) return;
+
+    for (final item in jsonList) {
+      this.items.add(new Invernadero.fromJson(item));
+    }
+  }
 
   factory Invernadero.fromJson(Map<String, dynamic> json) => Invernadero(
         idInvernadero: json["id_invernadero"],
         nombreInvernadero: json["nombre_invernadero"],
         idEstado: json["id_estado"],
-        productosSeleccionados: json["productos"].length > 0
-            ? List<int>.from(
-                json["productos"].map((item) => item["id_producto"]))
-            : [],
+        productosSeleccionados:
+            json.containsKey("productos") && json["productos"].length > 0
+                ? List<int>.from(
+                    json["productos"].map((item) => item["id_producto"]))
+                : [],
       );
 
   Map<String, dynamic> toJson() => {
@@ -45,5 +55,32 @@ class Invernadero {
         "nombre_invernadero": nombreInvernadero,
         "productos_seleccionados": productosSeleccionados,
         "productos_no_seleccionados": productosNoSeleccionados
+      };
+}
+
+InvernaderoFilter invernaderoFilterFromJson(String str) =>
+    InvernaderoFilter.fromJson(json.decode(str));
+
+String invernaderoFilterToJson(InvernaderoFilter data) =>
+    json.encode(data.toJson());
+
+class InvernaderoFilter {
+  int pagina;
+  int filas;
+
+  InvernaderoFilter({
+    this.pagina = 1,
+    this.filas = 10,
+  });
+
+  factory InvernaderoFilter.fromJson(Map<String, dynamic> json) =>
+      InvernaderoFilter(
+        pagina: json["pagina"],
+        filas: json["filas"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "pagina": pagina,
+        "filas": filas,
       };
 }
