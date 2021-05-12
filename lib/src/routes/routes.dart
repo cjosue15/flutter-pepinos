74764b292b8db136315b24672a1c5aaae0b012e0
Carlos Morales <carlosjosuemn98@gmail.com>
2021-05-12 12:35:09 -0500

    refactor(routes): add new HomePage to initial route

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pepinos/src/pages/campanias/campanias_form_page.dart';
 import 'package:pepinos/src/pages/campanias/campanias_list_page.dart';
+import 'package:pepinos/src/pages/charts/home_page.dart';
 import 'package:pepinos/src/pages/clientes/cliente_form_page.dart';
 import 'package:pepinos/src/pages/clientes/clientes_list_page.dart';
 import 'package:pepinos/src/pages/invernaderos/invernadero_form_page.dart';
@@ -13,7 +14,8 @@ import 'package:pepinos/src/pages/ventas/ventas_list_page.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes() {
   return <String, WidgetBuilder>{
-    '/': (BuildContext context) => VentasPage(),
+    '/': (BuildContext context) => HomePage(),
+    'ventas': (BuildContext context) => VentasPage(),
     'ventas/form': (BuildContext context) => VentasForm(),
     'ventas/detail': (BuildContext context) => VentasDetailPage(),
     'clientes': (BuildContext context) => ClientesListPage(),
