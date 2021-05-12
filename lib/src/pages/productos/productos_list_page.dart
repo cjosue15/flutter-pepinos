2aba7201db218dad943d7d5be2829ef12ce740c2
Carlos Morales <carlosjosuemn98@gmail.com>
2021-05-12 17:29:40 -0500

    refactor(producto list): add custom widget Infinite List

--- lib/src/pages/productos/productos_list_page.dart
+++ lib/src/pages/productos/productos_list_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pepinos/src/models/paginacion_model.dart';
 import 'package:pepinos/src/models/producto_model.dart';
 import 'package:pepinos/src/providers/productos/productos_provider.dart';
-import 'package:pepinos/src/widgets/alert_dialog.dart';
 import 'package:pepinos/src/widgets/drawer_menu.dart';
+import 'package:pepinos/src/widgets/infinite_list_view.dart';
 
 class ProductosListPage extends StatefulWidget {
   @override
@@ -11,33 +11,42 @@ class ProductosListPage extends StatefulWidget {
 }
 
 class _ProductosListPageState extends State<ProductosListPage> {
-  final ProductosProvider _productosProvider = new ProductosProvider();
-  final CustomAlertDialog _customAlertDialog = new CustomAlertDialog();
-  ScrollController _scrollController = new ScrollController();
-  bool _isLoading = false;
-  int _nextPage = 1;
+  ProductosProvider _productosProvider = new ProductosProvider();
+  ProductoFilter _productoFilter = new ProductoFilter();
+  bool _isFetching = false;
+  bool _isInitialLoading = false;
+  bool _hasInitialError = false;
+  bool _hasErrorAfterFetching = false;
   Paginacion _paginacion = new Paginacion();
+  List<Producto> _productos = [];
 
   @override
   void initState() {
     super.initState();
-
-    _scrollController.addListener(_handleController);
-
-    _productosProvider.getAllProducts().then((response) {
+    setState(() {
+      _hasInitialError = false;
+      _isInitialLoading = true;
+    });
+    _productosProvider
+        .getAllProducts(productoFilter: _productoFilter)
+        .then((response) {
       setState(() {
         _paginacion = response['paginacion'];
+        _productos = response['productos'];
+        _hasInitialError = false;
+        _isInitialLoading = false;
       });
     }).catchError((onError) {
-      _productosProvider.disposeStream();
+      setState(() {
+        _hasInitialError = true;
+        _isInitialLoading = false;
+      });
     });
   }
 
   @override
   void dispose() {
     super.dispose();
-    _productosProvider?.disposeStream();
-    _productosProvider.token.cancel();
   }
 
   @override
@@ -47,45 +56,43 @@ class _ProductosListPageState extends State<ProductosListPage> {
         title: Text('Productos'),
       ),
       drawer: DrawerMenu(),
-      body: Stack(
-        children: <Widget>[
-          _createStreamBuilderProducts(context),
-          _createLoading()
-        ],
+      body: InfiniteListView(
+        context: context,
+        data: _productos,
+        length: _productos.length,
+        paginacion: _paginacion,
+        hasInitialError: _hasInitialError,
+        hasErrorAfterFetching: _hasErrorAfterFetching,
+        isFetching: _isFetching,
+        isInitialLoading: _isInitialLoading,
+        onScroll: (int pagina) async {
+          setState(() {
+            _hasErrorAfterFetching = false;
+            _isFetching = true;
+          });
+          try {
+            _productoFilter.pagina = pagina;
+            final response = await _productosProvider.getAllProducts(
+                productoFilter: _productoFilter);
+            _productos.addAll(response['productos']);
+            _paginacion = response['paginacion'];
+            _hasErrorAfterFetching = false;
+            _isFetching = false;
+            setState(() {});
+          } catch (e) {
+            setState(() {
+              _hasErrorAfterFetching = true;
+              _isFetching = false;
+            });
+          }
+        },
+        itemBuilder: (context, item, index) => _createItem(context, item),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _goFormPage(context, null),
       ),
     );
-  }
-
-  _createStreamBuilderProducts(BuildContext context) {
-    return StreamBuilder(
-      stream: _productosProvider.productosStream,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        return snapshot.hasData
-            ? _createListProduct(snapshot.data)
-            : snapshot.hasError
-                ? _customAlertDialog.showErrorInBuilders(context)
-                : Center(child: CircularProgressIndicator());
-      },
-    );
-  }
-
-  Widget _createListProduct(List<Producto> productos) {
-    return productos.length > 0
-        ? ListView.builder(
-            itemCount: productos.length,
-            itemBuilder: (BuildContext context, int index) {
-              return _createItem(context, productos[index]);
-            },
-          )
-        : Center(
-            child: Text(
-            'No hay resultados para productos.',
-            style: TextStyle(fontSize: 20),
-          ));
   }
 
   Widget _createItem(BuildContext context, Producto producto) {
@@ -106,49 +113,9 @@ class _ProductosListPageState extends State<ProductosListPage> {
     );
   }
 
-  Widget _createLoading() {
-    return _isLoading
-        ? Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-                Center(child: CircularProgressIndicator()),
-                SizedBox(
-                  height: 20.0,
-                )
-              ])
-        : Container();
-  }
-
   _goFormPage(BuildContext context, String idProducto) {
     Navigator.pushNamed(context, 'productos/form',
         arguments:
             idProducto == null || idProducto.isEmpty ? null : idProducto);
-  }
-
-  _handleController() async {
-    if (_scrollController.position.pixels ==
-        _scrollController.position.maxScrollExtent) {
-      if (_paginacion.pagSiguiente == null) return;
-      _nextPage++;
-      setState(() {
-        _isLoading = true;
-      });
-      try {
-        final response =
-            await _productosProvider.getAllProducts(pagina: _nextPage);
-        setState(() {
-          _paginacion = response['paginacion'];
-          _isLoading = false;
-        });
-        _scrollController.animateTo(_scrollController.position.pixels + 50,
-            duration: Duration(milliseconds: 450), curve: Curves.fastOutSlowIn);
-      } catch (e) {
-        _productosProvider.disposeStream();
-        setState(() {
-          _isLoading = false;
-        });
-      }
-    }
   }
 }
